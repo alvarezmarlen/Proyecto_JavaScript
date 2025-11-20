f45664206939dbd9734ff1e0bc94e977788fc604
alvarezmarlen <marlenjosefalvarez.com>
2025-11-20 13:18:45 +0100

    modificacion  quitar ejemplos muy estructurados

--- assets/js/metodos-seleccion.js
+++ assets/js/metodos-seleccion.js
@@ -16,58 +16,31 @@ cambio.style.color = "red";
 
 
 
-/* Ejemplo 4   Hover sobre el párrafo*/
-let parHover = document.getElementById("por-encima");
-parHover.addEventListener("mouseover", function() {
-    parHover.style.color = "blue"
-});
-
-
-document.getElementById("btnClick").addEventListener("click", function () {
-    document.getElementById("resClick").innerHTML = "Hiciste clic!";
-    this.innerHTML = "¡Gracias!"; // cambia el texto del botón
-});
-
-document.getElementById("nombre").addEventListener("input", function() {
-    document.getElementById("resInput").innerHTML = "Hola " + document.getElementById("nombre").value;
-});
-
 
 /* ------------------------------------------------------------------
             Document.getElementByClassName
 --------------------------------------------------------------------*/
 /* Ejemplo 1 */
-const elementos = document.getElementsByClassName("resaltado");
+const  explicacion = document.getElementsByClassName("nota");
+document.getElementById("demostra").innerHTML = 
+`El primer párrafo (índice 0) con la clase="nota" es: ` + explicacion[0].innerHTML;
 
-for (let i = 0; i < elementos.length; i++) {
-     elementos[i].style.color = "red";    
-}
 
 /* Ejemplo 2 */
-const notas = document.getElementsByClassName("nota");
-
-for (let i = 0; i < notas.length; i++) {
-     notas[i].textContent = "Texto modificado " + (i + 1);    
+const elementos = document.getElementsByClassName("resaltadoc");
+for (let i = 0; i < elementos.length; i++) {
+     elementos[i].style.color = "rgb(60, 221, 221";    
 }
-
-
-/* Ejemplo 3 */
-const items = document.getElementsByClassName("hoverItem");
-
-for (let i = 0; i < items.length; i++) {
-    items[i].addEventListener("mouseover", function() {
-        this.style.backgroundColor = "yellow";
-    });
-    items[i].addEventListener("mouseout", function() {
-        this.style.backgroundColor = "";
-    });
-}  
 
 
 
 /* ------------------------------------------------------------------
             Document.getElementsByTagName()
 --------------------------------------------------------------------*/
+const elementosTag = document.getElementsByTagName("p")
+document.getElementById("demoTag").innerHTML =
+`El primer párrafo (índice 1) de las etiquetas "p" es: ` + elementosTag[1].innerHTML;
+
 /* Ejemplo 1 */
 function cambiarColor() {
     //Selleciona todos los elementos p
